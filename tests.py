769d2761ff65077335c5c8dbbe56544550e1810f
Dmitry Vlasov <chilbkq@yandex.ru>
2024-08-27 23:48:23 +0400

    Тесты добавлены в файл test.py

--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
 from main import BooksCollector
-
+import pytest
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-#Провряем переменные в __init__
+    #Провряем переменные в __init__
     #Проверяем, что books_genre это пустой словарь
     def test_init_books_genre_true(self):
         books_init_collection = BooksCollector()
@@ -27,26 +27,20 @@ class TestBooksCollector:
         assert books_init_age_rating.genre_age_rating == ['Ужасы',
                                         'Детективы']
 
-#Проверяем метод add_new_book
+    #Проверяем метод add_new_book
     #Проверяем, что новая книга добавилась
     def test_add_new_book_true(self):
         collector = BooksCollector()
         collector.add_new_book('НеДжедайские техники')
         assert 'НеДжедайские техники' in collector.books_genre
-
+    @pytest.mark.parametrize('book_true', ['', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'])
     #Проверяем, что пустая книга не добавилась
-    def test_add_new_book_name_empty_name_true(self):
+    def test_add_new_book_name_is_not_validated_true(self, book_true):
         collector = BooksCollector()
-        collector.add_new_book('')
-        assert '' not in collector.books_genre
+        collector.add_new_book(book_true)
+        assert book_true not in collector.books_genre
 
-    #Провряем, что книга с длинным названием не добавилась
-    def test_add_new_book_name_long_name_true(self):
-        collector = BooksCollector()
-        collector.add_new_book('аааааааааааааааааааааааааааааааааааааааааааааааааа')
-        assert 'аааааааааааааааааааааааааааааааааааааааааааааааааа' not in collector.books_genre
-
-#Проверяем метод set_book_genre
+    #Проверяем метод set_book_genre
     #Проверяем, что жанр добавился к книге
     def test_set_book_genre_add_new_genre_true(self):
         collector = BooksCollector()
@@ -59,8 +53,6 @@ class TestBooksCollector:
         collector = BooksCollector()
         collector.add_new_book('НеДжедайские техники')
         collector.set_book_genre('НеДжедайские техники', 'Такого жанра нет')
-        #assert 'Такого жанра нет' not in collector.set_book_genre['НеДжедайские техники'] - Здесь, видимо, ошибка была, что я обращаюсь в assert к set_book_genre, а за вывод ведь отвечает get_book_genre
-        #Отславляю эту строчку лишь для того, чтобы если я что-то сделал не так, то меня поправили бы
         assert collector.get_book_genre('НеДжедайские техники') == ''
 
     #Проверяем, что пустой жанр не добавился к книге
@@ -68,47 +60,67 @@ class TestBooksCollector:
         collector = BooksCollector()
         collector.add_new_book('НеДжедайские техники')
         collector.set_book_genre('НеДжедайские техники', '')
-        assert collector.get_book_genre('НеДжедайские техники') == '' #Здесь была такая же ошибка. Если можно как-то по-другому проверить, то я буду счастлив, если намекнете. Просто я всегда думал, что методы должны как-то изолированны друг от друга проверяться
+        assert collector.get_book_genre('НеДжедайские техники') == ''
 
-#Проверяем метод get_book_genre
+    #Проверяем метод get_book_genre
     def test_get_book_genre_existing_book_with_genre_get_book_genre(self):
         collector = BooksCollector()
         collector.add_new_book('НеДжедайские техники')
         collector.set_book_genre('НеДжедайские техники', 'Ужасы')
-        assert collector.get_book_genre('НеДжедайские техники') == 'Ужасы'  # Ожидается True
+        assert collector.get_book_genre('НеДжедайские техники') == 'Ужасы'
 
     def test_get_book_genre_existing_book_no_genre_get_empty_genre(self):
         collector = BooksCollector()
         collector.add_new_book('НеДжедайские техники')
-        assert collector.get_book_genre('НеДжедайские техники') == ''  # Ожидается True
+        assert collector.get_book_genre('НеДжедайские техники') == ''
 
-#Проверяем метод get_books_with_specific_genre
-    #Метод показался мне что-то прям слишком сложным :(((
-    #Потратил всё время, которое у меня было, на решение технических проблем с терминалом Cygwin и связью PyCharm <-> Pytest
-    #Ввиду чего написал всего один тест, но и по заданию проекта достаточно всего 1 теста на метод :((( Извините :((((
+    #Проверяем метод get_books_with_specific_genre
+    #Проверяет, что метод возвращает книгу с указанным специальным жанром
+    def test_get_books_with_specific_genre_recieved_book_with_spec_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('НеДжедайские техники')
+        collector.set_book_genre('НеДжедайские техники', 'Ужасы')
+        assert collector.get_books_with_specific_genre('Ужасы') == ['НеДжедайские техники']
+    #Проверяет, что метод не возвращает книги других жанров
+    def test_get_books_with_specific_genre_other_genre_not_included(self):
+        collector = BooksCollector()
+        collector.add_new_book('НеДжедайские техники')
+        collector.add_new_book('Тестирование Дот NET')
+        collector.set_book_genre('НеДжедайские техники', 'Ужасы')
+        collector.set_book_genre('Тестирование Дот NET', 'Фантастика')
+        assert collector.get_books_with_specific_genre('Ужасы') == ['НеДжедайские техники']
 
     #Проверяет, что метод возвращает пустой список, если книги по данному жанру нет в коллекции
     def test_get_books_with_specific_genre_no_books_show_empty_list(self):
         collector = BooksCollector()
         assert collector.get_books_with_specific_genre('Ужасы') == []
-#Провреяем метод get_books_genre
-    #Здесь тоже решил пока что не заморачиваться, ввиду малого времени :((
-
+    #Провреяем метод get_books_genre
     #Проверяет, что метод возвращает пустой словарь, если в словарь не добавлена ни одна книга
     def test_get_books_genre_empty_dict(self):
         collector = BooksCollector()
         assert collector.get_books_genre() == {}
 
-#Проверяем метод get_books_for_children
-    # Здесь тоже всего один тест пока что :(((
-    # Постараюсь попозже добавить хоть что-нибудь ещё. Если не добавил, то не успел
+    #Проверяем метод get_books_for_children
+    #Проверяет, что метод возвращает книгу, подходящую для детей
+    def test_get_books_for_children_book_appear(self):
+        collector = BooksCollector()
+        collector.add_new_book('НеДжедайские техники')
+        collector.set_book_genre('НеДжедайские техники', 'Мультфильмы')
+        assert collector.get_books_for_children() == ['НеДжедайские техники']
 
+    def test_get_books_fir_children_only_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book('НеДжедайские техники')
+        collector.set_book_genre('НеДжедайские техники', 'Мультфильмы')
+        collector.add_new_book('Тестирование Дот NET')
+        collector.set_book_genre('Тестирование Дот NET', 'Ужасы')
+        assert collector.get_books_for_children() == ['НеДжедайские техники']
     #Проверяет, что метод возвращает пустой список, если книг вообще нет
     def test_get_books_for_children_no_books_empty_list(self):
         collector = BooksCollector()
         assert collector.get_books_for_children() == []
 
-#Проверяем метод add_book_in_favorites
+    #Проверяем метод add_book_in_favorites
     #Проверяет, что метод не добавляет несуществующую книгу в избранное
     def test_add_book_in_favorites_unknown_book_not_in_favorites(self):
         collector = BooksCollector()
@@ -132,7 +144,7 @@ class TestBooksCollector:
         collector.add_book_in_favorites('НеДжедайские техники')
         assert len(collector.favorites) == 2
 
-#Проверяем метод delete_book_from_favorites
+    #Проверяем метод delete_book_from_favorites
     #Проверяем, что метод удаляет книгу из избранного
     def test_delete_book_from_favorites_book_in_favorites(self):
         collector = BooksCollector()
@@ -145,13 +157,10 @@ class TestBooksCollector:
     def test_delete_book_from_favorites_delete_book_that_doesnt_exist_nothing_has_changed(self):
         collector = BooksCollector()
         collector.add_new_book('НеДжедайские техники')
-        #past_condition = list(collector.favorites)
-        #future_condition = collector.delete_book_from_favorites('НеДжедайские техники')
-        #assert past_condition == future_condition
         collector.delete_book_from_favorites('НеДжедайские техники')
         assert 'НеДжедайские техники' not in collector.favorites
 
-#Проверяем метод get_list_of_favorites_books
+    #Проверяем метод get_list_of_favorites_books
     #Провряем, что список возвращаем пустой список, если в нем нет ни одной книги
     def test_get_list_of_favorites_books_no_books_list_is_empty(self):
         collector = BooksCollector()
