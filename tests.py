8041035b2369eca3ff62a2976884715a6aebf511
Dmitry Vlasov <chilbkq@yandex.ru>
2024-08-27 23:51:51 +0400

    Исправлены тесты в tests.py

--- tests.py
+++ tests.py
@@ -172,3 +172,4 @@ class TestBooksCollector:
         collector.add_new_book('НеДжедайские техники')
         collector.add_book_in_favorites('НеДжедайские техники')
         assert collector.get_list_of_favorites_books() == ['НеДжедайские техники']
+
